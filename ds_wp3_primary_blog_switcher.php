efc3a45eb562ead96b06e0b9b18520928956677a
dsader <dsader@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-16 20:34:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/primary-blog-switcher-for-superadmins/trunk@276892 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ds_wp3_primary_blog_switcher.php
@@ -0,0 +1,166 @@
+<?php
+/*
+Plugin Name: Primary Blog Switcher for SuperAdmins
+Plugin URI: http://wordpress.org/extend/plugins/primary-blog-switcher-for-superadmins/
+Description: Adds a dropdown primary blog switcher to a user's profile at SuperAdmin->Users->Edit. Users with blog 1 as their primary appear in an admin notice visible only to the SuperAdmin->Admin page.
+Author: D Sader
+Version: 3.0.1
+Author URI: http://dsader.snowotherway.org
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+*/
+class ds_primary_blog_switcher {
+
+	function ds_primary_blog_switcher() {
+		
+		}
+
+	function admin_notices() {
+		if( !is_super_admin() ) return;
+		global $wpdb;
+			$query = "SELECT * FROM {$wpdb->users} WHERE ID !='1'"; // exclude "admin" user
+			$user_ids = $wpdb->get_results( $query, ARRAY_A );
+			if( $user_ids )	{
+				$list	= array();
+				foreach($user_ids as $user_id) {
+					$primary_blog = get_usermeta($user_id['ID'],'primary_blog');
+					if ($primary_blog == '1' || !$primary_blog) { // or "Dashboard blog" or no blog at all
+						$url = clean_url( "user-edit.php?user_id=".$user_id['ID'] );
+					$list[]	= '<a class="delete" href="' . $url . '">' . $user_id['user_login'] . '</a>';
+					}
+				}
+				$count = count($list);
+				echo '<div id="update-nag">The following '.$count.' users list blog 1 as their primary blog at <a href="'.admin_url().'wpmu-users.php">Site Admin->Users</a>: ';
+				if ($count)  
+				echo implode(' | ', $list); 
+				echo '</div>';
+			}	
+	}
+	
+	function choose_primary_blog() {
+		$edit_user = (int) $_GET['user_id'];	
+		?>
+		<table class="form-table">
+		<tr>
+			<th scope="row"><?php _e('Primary Blog'); ?></th>
+			<td>
+			<?php
+			$all_blogs = get_blogs_of_user( $edit_user );
+			
+			if( count( $all_blogs ) > 1 ) {
+				$found = false;
+			?>
+			<select name="primary_blog">
+				<?php if( $primary_blog = get_usermeta( $edit_user, 'primary_blog' ) ) { ?>
+				<optgroup label="Primary Blog">
+			<?php
+				foreach( (array) $all_blogs as $blog ) { 
+					if( $primary_blog == $blog->userblog_id ) {
+						$found = true; 
+			?>
+					<option value='<?php echo $blog->userblog_id ?>'<?php if( $primary_blog == $blog->userblog_id ) echo ' selected="selected"' ?>><?php echo esc_url( get_home_url( $blog->userblog_id ) ) ?></option>
+			<?php 
+					}
+				} 
+			?>
+				</optgroup>
+				<?php } ?>
+				<optgroup label="User Blogs">
+			<?php
+			if($primary_blog) unset($all_blogs[$primary_blog]);
+
+				foreach( (array) $all_blogs as $blog ) {
+					if($blog) {
+			?>
+				<option value='<?php echo $blog->userblog_id ?>'><?php echo esc_url( get_home_url( $blog->userblog_id ) ) ?></option>
+				<?php } else { 
+				echo "N/A";
+				}
+				}
+				?>
+				</optgroup>
+			<?php
+				if( get_sitewide_tags_option( 'tags_blog_enabled' ) == '1' ) {
+				 if( $tags_blog_id = get_sitewide_tags_option( 'tags_blog_id' ) ) { 
+					$tags_blog = get_blog_details($tags_blog_id); ?>
+				<optgroup label="Tags Blog">
+				<option value='<?php echo $tags_blog_id ?>'><?php echo esc_url( get_home_url( $tags_blog_id ) ) ?></option>
+				</optgroup>
+			<?php } 
+				}	
+			if ( $dashboard_blog_id = get_site_option( 'dashboard_blog' ) ) {
+					$dashboard_blog = get_blog_details( $dashboard_blog_id ); 
+?>
+				<optgroup label="Dashboard Blog">
+				
+				<option value='<?php echo $dashboard_blog_id ?>'><?php echo esc_url( get_home_url( $dashboard_blog_id ) ) ?></option>
+				</optgroup>
+<?php } 
+
+/*
+
+			// "special blog" add $special_blog_id to add user to some, well, special blog.
+
+				<optgroup label="Other Blogs"></optgroup>
+				<optgroup label="Special Blog">
+				<?php $special_blog_id = '63'; //
+				$special_blog = get_blog_details( $special_blog_id ); ?>
+				<option value='<?php echo $special_blog_id ?>'>http://<?php echo $special_blog->domain.$special_blog->path ?></option>
+				</optgroup>
+
+*/
+
+?>						
+			</select>
+			<?php
+			if( !$found ) {
+				$blog = array_shift( $all_blogs );
+				update_user_meta( $edit_user, 'primary_blog', $blog->userblog_id );
+				}
+			} elseif( count( $all_blogs ) == 1 ) {
+				$blog = array_shift( $all_blogs );
+				echo $blog->domain;
+				if( $primary_blog != $blog->userblog_id ) // Set the primary blog again if it's out of sync with blog list.
+				update_user_meta( $edit_user, 'primary_blog', $blog->userblog_id );
+			} else {
+				echo "N/A";
+		}
+		?>
+		</td>
+	</tr>
+	</table>
+	<?php	
+	}
+	
+	function profile_update_primary_blog() {
+		global $user_id;
+		$blog_id = $_POST['primary_blog'];
+		if ( isset( $blog_id ) ) {	
+			update_user_meta( $user_id, 'primary_blog', $blog_id );
+			$is_member = is_user_member_of_blog( $user_id, $blog_id );
+			if(!$is_member) 
+				add_user_to_blog($blog_id, $user_id, 'subscriber');
+		}
+	}
+}
+
+if (class_exists("ds_primary_blog_switcher")) {
+	$ds_primary_blog_switcher = new ds_primary_blog_switcher();	
+}
+
+if (isset($ds_primary_blog_switcher)) {
+	add_action( 'edit_user_profile', array(&$ds_primary_blog_switcher, 'choose_primary_blog') );
+	add_action( 'edit_user_profile_update',  array(&$ds_primary_blog_switcher, 'profile_update_primary_blog') );
+	add_action( 'mu_rightnow_end', array(&$ds_primary_blog_switcher, 'admin_notices'));
+
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,64 @@
+=== Primary Blog Switcher for SuperAdmins ===
+Contributors: dsader
+Donate link: http://dsader.snowotherway.org
+Tags: multisite, network, primary blog, primary site, profile, my blogs, primary blog switcher, edit users,
+Requires at least: 3.0
+Tested up to: 3.0.1
+Stable tag: Trunk
+
+WP3.0 multisite "mu-plugin" to allow SuperAdmin to set the "Primary Blog" (aka Primary Site) of a user while editing a profile.
+
+== Description ==
+WP3.0 multisite "mu-plugin" to allow SuperAdmin to set the "Primary Blog" (aka Primary Site) of a user while editing a user's profile.
+
+Well, for whatever reasons (usually users fiddling around - I use WP3 multisite in a school with students grades 4-12), users aren't attached(or become unattached) to the correct "Primary Blog". 
+
+This isn't a deal breaker, but annoying when they login and are redirected to a blog that is not their expected primary. It also is annoying when I use other plugins to list user primary blog for display in a member directory, member profiles, etc.
+
+Telling users to reset their primary blog at their own Dashboard->My Blogs is a fix, but the SuperAdmin(Teacher in my case) can head off the confusion first with this plugin. There is no other way(AFAIK) for the SuperAdmin to set the "Primary Blog" of a user while editing their profile. 
+
+Now, I can quickly scan the SuperAdmin list of users and edit profiles and set primary blogs of any user correctly.
+
+I can also use my <a href="http://wordpress.org/extend/plugins/menus/">Menus plugin</a> to toggle the My Sites menu item so users can no longer fiddle with the Primary Site switcher at all. Problem solved.
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `ds_wp3_primary_blog_switcher.phpp` to the `/wp-content/mu-plugins/` directory
+2. Edit user profiles as SuperAdmin Dashboard->SuperAdmin->Users->Edit
+3. View Dashboard->SuperAdmin->Admin for notices of users who have the main blog set as their primary blog.
+
+== Frequently Asked Questions ==
+
+* Can I set the Main blog as a user's primary? Yes.
+
+* Can I set the Dashboard blog (if enabled) as a user's primary? Yes.
+
+* Can I set Donncha's Sitewide Tags blog (if enabled) as a user's primary? Yes.
+
+* Can I add a user to some other blog "special blog" as their primary? Yes, but see "special blog" comments in the plugin code.
+
+* Does this plugin filter the list of blogs already listed at a user's Dashboard->My Sites->Primary Site? No.
+
+
+== Screenshots ==
+
+
+== Notes ==
+
+The original code for the Primary Site switcher is in wp-admin-includes/ms.php. I've basically copied that, but changed `get_current_user_id()` to `$edit_user = (int) $_GET['user_id'];` and added it to the "edit_user_profile" hook.
+
+
+== Changelog ==
+
+= 3.0.1 =
+
+Initial Release for WP3.x multisite
+
+== Upgrade Notice ==
+
+= 3.0.1 =
+WPMU2.9.2 version no longer supported.
+
